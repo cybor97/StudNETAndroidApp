73759c850f56ca9f976d38aa6f73c325eb69d55e
[CYBOR] <muhametshin.1997@gmail.com>
2017-11-17 00:36:50 +0300

    Found out how to effectively work with AndroidAsyncHttp http client.

--- app/src/main/java/com/cybor/studnet/activities/PreloaderActivity.java
+++ app/src/main/java/com/cybor/studnet/activities/PreloaderActivity.java
@@ -1,28 +1,68 @@
 package com.cybor.studnet.activities;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import com.cybor.studnet.R;
 import com.cybor.studnet.data.Configuration;
-import com.cybor.studnet.data.Lesson;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.SyncHttpClient;
+import com.loopj.android.http.TextHttpResponseHandler;
 
+import org.json.JSONArray;
+
+import java.io.UnsupportedEncodingException;
 import java.util.concurrent.Executors;
 
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.StringEntity;
 import io.realm.Realm;
-
-import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP;
-import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 public class PreloaderActivity extends Activity {
     private static final int LESSONS_COUNT = 7;
+    private static final String HOST_URL = "http://192.168.0.106";
+    private static final String AUTH_URL = HOST_URL + "/auth/signIn";
+    private static final String SCHEDULE_URL = HOST_URL + "/data/getScheduleRecords";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.preloader_layout);
+
+        AsyncHttpResponseHandler authResponseHandler = new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+
+            }
+        };
+        JsonHttpResponseHandler scheduleResponseHandler = new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                super.onSuccess(statusCode, headers, response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+
+            @Override
+            public void onFinish() {
+//                            startActivity(new Intent(PreloaderActivity.this, MainActivity.class)
+//                                    .addFlags(FLAG_ACTIVITY_NEW_TASK | FLAG_ACTIVITY_CLEAR_TOP));
+                super.onFinish();
+//                            PreloaderActivity.this.finish();
+            }
+        };
+        SyncHttpClient client = new SyncHttpClient();
 
         Executors.newSingleThreadExecutor()
                 .execute(() ->
@@ -30,17 +70,18 @@ public class PreloaderActivity extends Activity {
                     Realm.init(getApplicationContext());
 
                     Realm realm = Realm.getDefaultInstance();
+                    Configuration configuration = Configuration.getInstance(realm);
                     //if (realm.where(Lesson.class).count() == 0)
-                    Lesson.generateLessons(LESSONS_COUNT, Configuration.getInstance(realm), realm);
-
+                    //TODO:Implement data fetch
                     try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+                        client.post(PreloaderActivity.this, AUTH_URL,
+                                new StringEntity("{\"userName\":\"Cybor\", \"password\":\"unknown0\"}"),
+                                "application/json", authResponseHandler);
+                    } catch (UnsupportedEncodingException e) {
                     }
-                    startActivity(new Intent(this, MainActivity.class)
-                            .addFlags(FLAG_ACTIVITY_NEW_TASK | FLAG_ACTIVITY_CLEAR_TOP));
-                    finish();
+
+                    client.get(SCHEDULE_URL, new RequestParams(
+                            "scheduleId", 1, "weekday", 2, "isEven", 0), scheduleResponseHandler);
                 });
     }
 }
