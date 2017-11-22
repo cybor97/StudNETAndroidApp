b391d48217fabf267ad51d6843998b2ff3d53baa
[CYBOR] <muhametshin.1997@gmail.com>
2017-11-22 22:58:22 +0300

    Implemented Preloader logic.

--- app/src/main/java/com/cybor/studnet/activities/PreloaderActivity.java
+++ app/src/main/java/com/cybor/studnet/activities/PreloaderActivity.java
@@ -1,6 +1,7 @@
 package com.cybor.studnet.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
@@ -8,24 +9,24 @@ import com.cybor.studnet.APIClient;
 import com.cybor.studnet.APIResponseHandler;
 import com.cybor.studnet.R;
 import com.cybor.studnet.data.Configuration;
+import com.cybor.studnet.data.ScheduleRecord;
 import com.loopj.android.http.RequestParams;
 
+import java.util.Arrays;
 import java.util.concurrent.Executors;
 
-import cz.msebera.android.httpclient.Header;
 import io.realm.Realm;
 
+import static com.cybor.studnet.APIClient.GET_SCHEDULE;
+
 public class PreloaderActivity extends Activity implements APIResponseHandler {
-    private static final int LESSONS_COUNT = 7;
-    private static final String HOST_URL = "http://192.168.0.106";
-    private static final String AUTH_URL = HOST_URL + "/auth/signIn";
-    private static final String SCHEDULE_URL = HOST_URL + "/data/getScheduleRecords";
+    private APIClient apiClient;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.preloader_layout);
-        APIClient.init(getApplicationContext());
+        apiClient = APIClient.init(getApplicationContext());
 
         Executors.newSingleThreadExecutor()
                 .execute(() ->
@@ -33,20 +34,26 @@ public class PreloaderActivity extends Activity implements APIResponseHandler {
                     Realm.init(getApplicationContext());
 
                     Realm realm = Realm.getDefaultInstance();
-                    Configuration configuration = Configuration.getInstance(realm);
+                    Configuration.getInstance(realm);
 
                 });
-        APIClient.getInstance().get(SCHEDULE_URL,
-                new RequestParams("scheduleId", 1, "weekday", 2, "isEven", 0), this);
+        apiClient.get(0, GET_SCHEDULE,
+                new RequestParams("scheduleId", 1), this);
     }
 
     @Override
-    public void onSuccess(int statusCode, Header[] headers, String response) {
-
+    public void onSuccess(int requestId, int statusCode, String response) {
+        if (requestId == 0 && statusCode == 200) {
+            Realm.getDefaultInstance().executeTransaction(_realm -> _realm
+                    .copyToRealmOrUpdate(Arrays.asList(apiClient.getGson().fromJson(response, ScheduleRecord[].class))));
+            startActivity(new Intent(this, MainActivity.class)
+                    .putExtra("upToDate", true));
+        }
     }
 
     @Override
-    public void onError(int statusCode, Header[] headers, String response, Throwable error) {
-
+    public void onError(int requestId, int statusCode, String response, Throwable error) {
+        if (statusCode == 403)
+            startActivity(new Intent(this, AuthActivity.class));
     }
 }
