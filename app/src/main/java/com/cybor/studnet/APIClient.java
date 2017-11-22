3cbb41dc7ecb44084da0a54d4bfb2e8a1500c769
[CYBOR] <muhametshin.1997@gmail.com>
2017-11-22 23:04:53 +0300

    Made possible to do multiple requests from same Activity and differ results.

--- app/src/main/java/com/cybor/studnet/APIClient.java
+++ app/src/main/java/com/cybor/studnet/APIClient.java
@@ -2,6 +2,8 @@ package com.cybor.studnet;
 
 import android.content.Context;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.PersistentCookieStore;
 import com.loopj.android.http.RequestParams;
@@ -12,52 +14,63 @@ import cz.msebera.android.httpclient.HttpEntity;
 
 public class APIClient {
 
+    public static final String HOST_URL = "http://192.168.0.106";
+    public static final String SIGN_IN = "/auth/signIn";
+    public static final String SIGN_UP = "/auth/signUp";
+    public static final String GET_SCHEDULE = "/data/getScheduleRecords";
     private static APIClient instance;
     private AsyncHttpClient client;
-    private Context context;
+    private Gson gson;
 
     private APIClient(Context context) {
         this.client = new AsyncHttpClient();
-        this.context = context;
 
         client.setCookieStore(new PersistentCookieStore(context));
-        client.setUserAgent("API/StudNET " + BuildConfig.VERSION_NAME);
+        client.setUserAgent("API/StudNET/AndroidApp/" + BuildConfig.VERSION_NAME);
+        client.setMaxRetriesAndTimeout(10, 1000);
+
+        gson = new GsonBuilder().serializeNulls().create();
     }
 
-    public static void init(Context context) {
-        instance = new APIClient(context);
+    public static APIClient init(Context context) {
+        return instance = new APIClient(context);
     }
 
     public static APIClient getInstance() {
         return instance;
     }
 
-    public void get(String url, RequestParams params, APIResponseHandler responseHandler) {
-        client.get(url, params, new TextHttpResponseHandler() {
+    public Gson getGson() {
+        return gson;
+    }
+
+    public void get(int requestId, String method, RequestParams params, APIResponseHandler responseHandler) {
+        client.get(HOST_URL + method, params, new TextHttpResponseHandler() {
             @Override
             public void onFailure(int status, Header[] headers, String response, Throwable error) {
-                responseHandler.onError(status, headers, response, error);
+                responseHandler.onError(requestId, status, response, error);
             }
 
             @Override
             public void onSuccess(int status, Header[] headers, String response) {
-                responseHandler.onSuccess(status, headers, response);
+                responseHandler.onSuccess(requestId, status, response);
             }
         });
     }
 
-    public void post(String url, HttpEntity body, String contentType, APIResponseHandler responseHandler) {
-        client.post(context, url, body, contentType, new TextHttpResponseHandler() {
-            @Override
-            public void onFailure(int status, Header[] headers, String response, Throwable error) {
-                responseHandler.onError(status, headers, response, error);
-            }
+    public void post(Context context, int requestId, String method, HttpEntity body, APIResponseHandler responseHandler) {
+        client.post(context, HOST_URL + method, body, body.getContentType().getValue(),
+                new TextHttpResponseHandler() {
+                    @Override
+                    public void onFailure(int status, Header[] headers, String response, Throwable error) {
+                        responseHandler.onError(requestId, status, response, error);
+                    }
 
-            @Override
-            public void onSuccess(int status, Header[] headers, String response) {
-                responseHandler.onSuccess(status, headers, response);
-            }
-        });
+                    @Override
+                    public void onSuccess(int status, Header[] headers, String response) {
+                        responseHandler.onSuccess(requestId, status, response);
+                    }
+                });
     }
 
 }
--- app/src/main/java/com/cybor/studnet/APIResponseHandler.java
+++ app/src/main/java/com/cybor/studnet/APIResponseHandler.java
@@ -1,9 +1,7 @@
 package com.cybor.studnet;
 
-import cz.msebera.android.httpclient.Header;
-
 public interface APIResponseHandler {
-    void onSuccess(int statusCode, Header[] headers, String response);
+    void onSuccess(int requestId, int statusCode, String response);
 
-    void onError(int statusCode, Header[] headers, String response, Throwable error);
+    void onError(int requestId, int statusCode, String response, Throwable error);
 }
